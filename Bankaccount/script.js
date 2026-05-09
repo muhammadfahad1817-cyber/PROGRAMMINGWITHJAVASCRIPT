95d0c5878566a0aebcc19ce67901aca4494ec075
asp <asp@aptech.com>
2026-05-09 09:55:58 +0500

    update

--- Bankaccount/script.js
+++ Bankaccount/script.js
@@ -13,11 +13,31 @@ class BankAccount{
     }
 
     deposit(amount){
+        if(amount<=0){
+            console.log("Enter a valid amount")
+            return
+        }
         this.balance += amount
         console.log(`${this.accountNumber} is credited with ${amount}. Current balance: ${this.balance}`)
+    }
+    
+    withdraw(amount){
+        if(amount > this.balance){
+            console.log("Insufficient balance")
+            return
+        }
+        this.balance -= amount
+         console.log(`${this.accountNumber} is debited with ${amount}. Current balance: ${this.balance}`)
+
+    }
+
+    printstatement(){
+        console.log(`Dear ${this.holdername}, Your account ${this.accountNumber}. Available Balance: ${this.balance}`)
     }
 }
 
 let customer1 = new BankAccount("Ali","42401-24343-434",5000)
 customer1.displayinfo()
-customer1.deposit(5000)
+customer1.deposit(1)
+customer1.withdraw(6001)
+customer1.printstatement()
