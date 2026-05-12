b4bf50bb3dcdf1979ddd03540eabaaf7e8ad1a61
asp <asp@aptech.com>
2026-05-12 10:49:33 +0500

    update

--- sync&async/script.js
+++ sync&async/script.js
@@ -1,16 +1,41 @@
-// console.log("Task 1")
-// setTimeout(()=>{console.log ("Task 2 done after 2 seconds")},2000)
-// console.log("Task 3")
+// // console.log("Task 1")
+// // setTimeout(()=>{console.log ("Task 2 done after 2 seconds")},2000)
+// // console.log("Task 3")
 
-function greetuser (name, callback){
-    console.log(`Hello ${name}`)
-    callback()
-}
+// function greetuser (name, callback){
+//     console.log(`Hello ${name}`)
+//     callback()
+// }
 
-function aftergreet (){
-    console.log ("welcome to our website")
-}
+// function aftergreet (){
+//     console.log ("welcome to our website")
+// }
 
 
-greetuser("Ali", aftergreet)
-// aftergreet()
+// greetuser("Ali", aftergreet)
+// // aftergreet()
+
+// Promise
+// 1:pending
+// 2:resolve(fulfill)
+// 3:rejected(not fulfill)
+
+// promise -> class
+// create object
+
+// let promiseobj = new Promise ((resolve, rejected)=>{
+//     // console.log("Hey i am a promise")
+//     rejected("Try again")
+// })
+
+
+let promiseobj = new Promise ((resolve,reject)=>{
+    let success = true
+    setTimeout(()=>{
+        if(success){
+            resolve("Promise is fulfilled by shakeel")
+        }else{
+            reject("Shakeel dhokay bazz")
+        }
+    },2000)
+})
