82135dd5e7f3916d5d71695dead8932924d75a9a
asp <asp@aptech.com>
2026-05-12 09:51:40 +0500

    update work

--- sync&async/script.js
+++ sync&async/script.js
@@ -1,3 +1,15 @@
 console.log("Task 1")
 setTimeout(()=>{console.log ("Task 2 done after 2 seconds")},2000)
 console.log("Task 3")
+
+function greetuser (name){
+    console.log(`Hello ${name}`)
+}
+
+function aftergreet (){
+    console.log ("welcome to our website")
+}
+
+
+greetuser("Ali")
+aftergreet()
